df73328351954696418f24c1d2bba5c8e8d0b35b
oleg_pykulytsky <pykulytsky@gmail.com>
2020-11-05 18:00:16 +0200

    Added complete JWT auth app, changed README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+=====
+DRF JWT Authentication(Json Web Token)
+=====
+
+This is a Django app to add JWT authentication to your DRF project. 
+
+Quick start
+-----------
+1. Move 'authentication' directory to your project directory.
+
+2. Add "authentication" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'authentication',
+    ]
+
+
+3. Put AUTH_USER_MODEL = 'authentication.User' to your settings file.
+
+4. Run ``python manage.py migrate`` to create user model.
---
+++ authentication/api/views.py
@@ -0,0 +1,40 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework .views import APIView
+
+from authentication.models import User
+from authentication.api.serializers import LoginSerializer, RegistrationSerializer
+
+
+class RegistrationAPIView(APIView):
+    permission_classes = (AllowAny, )
+    serializer_class = RegistrationSerializer
+
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({
+            'token': serializer.data.get('token', None)
+        })
+    
+
+    def get(self, request):
+        return Response({
+            'info': 'for registration push POST request with "email", "username","password" body fields.'
+        })
+
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny, )
+    serializer_class = LoginSerializer
+
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
